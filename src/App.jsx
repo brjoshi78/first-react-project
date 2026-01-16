364343b9380451910154657be2cde6e9c1682644
= <=>
2026-01-16 12:55:29 +0530

    changed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 
-// import UseStateBasics from "./pages/UseStateBasics";
-import UseEffectBasics from "./pages/UseEffectBasics";
-
 const App = () => {
-  return <UseEffectBasics />;
+  return <div>App</div>;
 };
 
 export default App;
